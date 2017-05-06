e41adde1a2881e28af0ace39b5a7bb55bfa8ebec
harshbhavsar <harsh.bhavsar@copart.com>
2017-05-06 12:54:51 -0500

    Initial Commit

---
+++ src/main/java/com/par/system/PARSystemApplication.java
@@ -0,0 +1,13 @@
+package com.par.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PARSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PARSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/par/system/PARSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.par.system;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PARSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
